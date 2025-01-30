567d7f85e69ba86fda34e1f2cee9517d54b0ce13
Philip <philipchryssochoos@gmail.com>
2025-01-30 15:53:09 -0500

    create Order model

--- movies/models.py
+++ movies/models.py
@@ -19,3 +19,11 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return str(self.id) + ' - ' + self.movie.name
+    
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    total = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.id) + ' - ' + self.user.username
