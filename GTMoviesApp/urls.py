6b10100fd97728ad4ecf2c0633ae11db38ba8370
Philip <philipchryssochoos@gmail.com>
2025-01-30 16:45:36 -0500

    fix comma

--- GTMoviesApp/urls.py
+++ GTMoviesApp/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
     path('movies/', include('movies.urls')),
 ]
 
