ca54ca0537e73900564ccaae9115a02b6a42783c
aidendowling <148834853+aidendowling@users.noreply.github.com>
2025-02-06 16:06:28 -0500

    Added orders page so users can see their previous transaction history

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
 def signup(request):
     template_data = {}
     template_data['title'] = 'Sign Up'
@@ -17,3 +20,11 @@ def signup(request):
             template_data['form'] = form
             return render(request, 'accounts/signup.html',
                 {'template_data': template_data})
+
+@login_required
+def orders(request):
+    template_data = {}
+    template_data['title'] = 'Orders'
+    template_data['orders'] = request.user.order_set.all()
+    return render(request, 'accounts/orders.html',
+        {'template_data': template_data})
