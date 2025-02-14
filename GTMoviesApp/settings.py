24857ff9f377ea96998d0970387dd19ab91c2bc0
aidendowling <148834853+aidendowling@users.noreply.github.com>
2025-02-13 22:32:40 -0500

    Added reset password feature and linked it to the login page

--- GTMoviesApp/settings.py
+++ GTMoviesApp/settings.py
@@ -133,3 +133,12 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gtmovies@zoho.com'
+EMAIL_HOST_PASSWORD = 'Cheesedream83'
+
+
